8d88fba9b35d29810ae1c161ccc23fcf775531f2
Levin <levinmaksim263@gmail.com>
2023-09-28 22:11:01 +0700

    Вторая попытка

--- arifmetic.py
+++ arifmetic.py
@@ -5,6 +5,8 @@ b = float(input("Введите 2: "))
 
 z1 = ((math.cos(a)-math.cos(b))** 2) - ((math.sin(a)-math.sin(b))**2)
 print ("Z1: " , z1)
-z2 = (-4 * math.sin((a -b)/ 2)) * math.cos(a + b)
-
-print ("Z2: " , z2)
+#z2 = (-4 * math.sin((a -b)/ 2)) * math.cos(a + b)
+z4 = (a + b)
+z2 = (a - b) / 2
+z3 = (-4 * math.sin(z2)) * math.cos(z4)
+print ("Z2: " , z3)
